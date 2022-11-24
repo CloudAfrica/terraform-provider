6cb04e899c1056ee5937bcb99533deeeaf05af14
Andre Helberg <helberg.andre@gmail.com>
2022-11-24 10:07:45 +0200

    Clean up server model

--- cloudafrica/server_model.go
+++ cloudafrica/server_model.go
@@ -2,6 +2,8 @@ package cloudafrica
 
 import (
 	"github.com/hashicorp/terraform-plugin-framework/types"
+
+	"github.com/CloudAfrica/client"
 )
 
 // serversModel maps servers schema data.
@@ -13,4 +15,30 @@ type ServerModel struct {
 	RamMiB  types.Int64       `tfsdk:"ram_mib"`
 	SSHKeys []SSHKeyModel     `tfsdk:"ssh_keys"`
 	Disks   []ServerDiskModel `tfsdk:"disks"`
+}
+
+func ServerModelFromApi(server cloudafrica.ListServers200ResponseServersInner) ServerModel {
+	s := ServerModel{
+		ID:     types.Int64Value(int64(server.Id)),
+		Name:   types.StringValue(server.Name),
+		State:  types.StringValue(server.State),
+		CPUs:   types.Int64Value(int64(server.Cpus)),
+		RamMiB: types.Int64Value(int64(server.RamMib)),
+	}
+
+	for _, disk := range server.Disks {
+		s.Disks = append(s.Disks, ServerDiskModel{
+			ID: types.Int64Value(disk.Id),
+		})
+	}
+
+	for _, key := range server.SshKeys {
+		s.SSHKeys = append(s.SSHKeys, SSHKeyModel{
+			ID:   types.Int64Value(key.Id),
+			Name: types.StringValue(key.Name),
+			Body: types.StringValue(key.Body),
+		})
+	}
+
+	return s
 }
--- cloudafrica/server_resource.go
+++ cloudafrica/server_resource.go
@@ -2,12 +2,11 @@ package cloudafrica
 
 import (
 	"context"
-	"fmt"
-
 	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
-	"github.com/hashicorp/terraform-plugin-framework/types"
+	"strconv"
 )
 
 var (
@@ -29,57 +28,47 @@ func (d *ServerResource) Metadata(_ context.Context, req resource.MetadataReques
 
 func (d *ServerResource) GetSchema(_ context.Context) (tfsdk.Schema, diag.Diagnostics) {
 	return tfsdk.Schema{
-		Attributes: map[string]tfsdk.Attribute{
-			"servers": {
-				Computed: true,
-				Attributes: tfsdk.ListNestedAttributes(map[string]tfsdk.Attribute{
-					"id": {
-						Type:     types.Int64Type,
-						Computed: true,
-					},
-					"name": {
-						Type:     types.StringType,
-						Required: true,
-					},
-				}),
-			},
-		},
+		Attributes: ServerSchema(),
 	}, nil
+}
+
+func (r *ServerResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	//resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
+	id, err := strconv.ParseInt(req.ID, 10, 64)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Unable to import  CloudAfrica Server. Could not convert import ID to int64",
+			err.Error(),
+		)
+		return
+	}
+	resp.Diagnostics.Append(resp.State.SetAttribute(ctx, path.Root("id"), id)...)
 }
 
 func (d *ServerResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
 	var state ServerModel
 
-	servers_resp, _, err := d.client.Client.ServerApi.ListServers(d.client.Auth).Execute()
+	diags := req.State.Get(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	serversResp, _, err := d.client.Client.ServerApi.GetServer(d.client.Auth, state.ID.ValueInt64()).Execute()
 	if err != nil {
 		resp.Diagnostics.AddError(
-			"Unable to Read CloudAfrica Servers",
+			"Unable to Read CloudAfrica Server",
 			err.Error(),
 		)
 		return
 	}
 
 	// Map response body to model
-	servers := servers_resp.Servers
-	fmt.Println(servers)
-	for _, server := range servers {
-		serverstate := ServerModel{
-			ID:   types.Int64Value(int64(server.Id)),
-			Name: types.StringValue(server.Name),
-		}
-
-		// TODO: disks and network
-		//for _, ingredient := range coffee.Ingredient {
-		//	serverstate.Ingredients = append(serverstate.Ingredients, serversIngredientsModel{
-		//		ID: types.Int64Value(int64(ingredient.ID)),
-		//	})
-		//}
-
-		state = serverstate
-	}
+	apiServer := serversResp.Server
+	state = ServerModelFromApi(apiServer)
 
 	// Set state
-	diags := resp.State.Set(ctx, &state)
+	diags = resp.State.Set(ctx, &state)
 	resp.Diagnostics.Append(diags...)
 	if resp.Diagnostics.HasError() {
 		return
